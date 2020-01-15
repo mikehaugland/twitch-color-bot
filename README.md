5092cd59d91582cdc3f68c8567ecfab85dc55496
Mike Haugland <mike.haugland@gmail.com>
2020-01-14 22:20:25 -0500

    feat(): Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Twitch Color Bot
+
+Setup the following environment variables:
+
+- `TWITCH_BOT_KEY` (required)
+- `TWITCH_BOT_USERNAME` (required)
+- `TWITCH_BOT_COLOR_MODE`
+  - Choose between `random` and `loop`
+  - Random will select a random color with varying hue, saturation, and lightness.
+  - Loop will increment the hue value through the spectrum and loop.
+- `TWITCH_BOT_INTERVAL`
+  - Time between color changes in seconds. Default 10 seconds.
+
+## Running the app
+
+Install dependencies with `yarn install`.
+
+Run app
+
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+const tmi = require("tmi.js");
+const convert = require("color-convert");
+const chalk = require("chalk");
+
+let hue = 0;
+let sat = 66;
+let light = 55;
+
+let connected = false;
+
+const colorMode = process.env.TWITCH_BOT_COLOR_MODE || "loop";
+
+if (!process.env.TWITCH_BOT_KEY || !process.env.TWITCH_BOT_USERNAME) {
+  console.log(
+    "TWITCH_BOT_KEY and TWITCH_BOT_USERNAME must be available as environment variables"
+  );
+  process.exit(1);
+}
+
+const client = new tmi.Client({
+  connection: {
+    reconnect: true,
+    secure: true
+  },
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_BOT_KEY
+  },
+  channels: [process.env.TWITCH_BOT_USERNAME.toLowerCase()]
+});
+
+client.on("connected", onConnected);
+client.on("disconnected", onDisconnected);
+
+client.connect();
+
+function onConnected(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+
+  connected = true;
+}
+
+function onDisconnected(reason) {
+  console.log(`* Disconnected: ${reason}`);
+
+  connected = false;
+}
+
+function generateRandomColor() {
+  hue = Math.floor(Math.random() * 360);
+  sat = 20 + Math.floor(Math.random() * 60);
+  light = 40 + Math.floor(Math.random() * 40);
+}
+
+function incrementColorLoop() {
+  if (hue < 351) {
+    hue += 2;
+  } else {
+    hue = 0;
+  }
+}
+
+function updateColor() {
+  if (colorMode === "random") {
+    generateRandomColor();
+  } else {
+    incrementColorLoop();
+  }
+
+  const color = convert.hsl.hex(hue, sat, light);
+  console.log(
+    `* Updating username color to`,
+    chalk.hex(`#${color}`)(`#${color}`)
+  );
+  client.color(`#${color}`);
+}
+
+const intervalTime = process.env.TWITCH_BOT_INTERVAL * 1000 || 10000;
+
+interval = setInterval(function() {
+  if (connected) {
+    updateColor();
+  }
+}, intervalTime);
